a79f14100ed1b571bd54bf5080295342aa3ec52a
carebare47 <tom_q@hotmail.co.uk>
2019-08-25 16:43:57 +0000

    minor changes, both wheels tested, both velocity estimates seem good

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -17,7 +17,7 @@
 float filterFrequency = 5.0;
 
 // create a one pole (RC) lowpass filter
-FilterOnePole lowpassFilter( LOWPASS, filterFrequency );   
+FilterOnePole lowpassFilter( LOWPASS, filterFrequency );
 
 
 
@@ -40,7 +40,12 @@ HardwareSerial Serial4(PA1, PA0);
 #define hall_A_int PA10
 #define hall_B_int PB3
 #define hall_C_int PB5
+
+#define r_hall_A_int PB4
+#define r_hall_B_int PA8
+#define r_hall_C_int PA9
 #define LED PA5
+
 
 
 //DigitalIn   hall_A_read(PA_10);
@@ -51,7 +56,10 @@ HardwareSerial Serial4(PA1, PA0);
 //InterruptIn hall_C_int(PB_5);
 
 volatile bool forward = false;
+volatile bool r_forward = false;
 volatile int counter = 0;
+
+volatile int r_counter = 0;
 
 void hall_a_rise()
 {
@@ -65,19 +73,16 @@ void hall_a_rise()
 
 void hall_a_change()
 {
-  static unsigned long last_interrupt_time = 0;
-  unsigned long interrupt_time = millis();
-  if (interrupt_time - last_interrupt_time > 10)
-  {
-    if (digitalRead(hall_A_int)) {
-      forward = (digitalRead(hall_B_int) ? true : false);
-    }
-    if (!digitalRead(hall_A_int)) {
-      forward = (digitalRead(hall_B_int) ? false : true);
-    }
-    counter += (forward ? 1 : (-1));
-    digitalWrite(LED, (forward ? HIGH : LOW));
+
+  if (digitalRead(hall_A_int)) {
+    forward = (digitalRead(hall_B_int) ? true : false);
   }
+  if (!digitalRead(hall_A_int)) {
+    forward = (digitalRead(hall_B_int) ? false : true);
+  }
+  counter += (forward ? 1 : (-1));
+
+  digitalWrite(LED, (forward ? HIGH : LOW));
 }
 
 void hall_b_change()
@@ -91,7 +96,7 @@ void hall_b_change()
     forward = (!digitalRead(hall_C_int) ? true : false);
   }
   counter += (forward ? 1 : (-1));
-  digitalWrite(PA5, forward);
+  digitalWrite(LED, (forward ? HIGH : LOW));
 }
 
 void hall_c_change()
@@ -103,6 +108,52 @@ void hall_c_change()
     forward = (digitalRead(hall_A_int) ? true : false);
   }
   counter += (forward ? 1 : (-1));
+  digitalWrite(LED, (forward ? HIGH : LOW));
+
+
+}
+
+
+void r_hall_a_change()
+{
+
+  if (digitalRead(r_hall_A_int)) {
+    r_forward = (digitalRead(r_hall_B_int) ? true : false);
+  }
+  if (!digitalRead(r_hall_A_int)) {
+    r_forward = (digitalRead(r_hall_B_int) ? false : true);
+  }
+  r_counter += (r_forward ? 1 : (-1));
+ // digitalWrite(LED, (r_forward ? HIGH : LOW));
+
+}
+
+void r_hall_b_change()
+{
+  if (digitalRead(r_hall_B_int))
+  {
+    r_forward = (!digitalRead(r_hall_C_int) ? false : true);
+  }
+  if (!digitalRead(r_hall_B_int))
+  {
+    r_forward = (!digitalRead(r_hall_C_int) ? true : false);
+  }
+  r_counter  += (r_forward ? 1 : (-1));
+  //digitalWrite(LED, (r_forward ? HIGH : LOW));
+
+}
+
+void r_hall_c_change()
+{
+  if (digitalRead(r_hall_C_int)) {
+    r_forward = (!digitalRead(r_hall_A_int) ? false : true);
+  }
+  if (!digitalRead(hall_B_int)) {
+    r_forward = (digitalRead(r_hall_A_int) ? true : false);
+  }
+  r_counter  += (r_forward ? 1 : (-1));
+  //digitalWrite(LED, (r_forward ? HIGH : LOW));
+
 }
 
 
@@ -119,11 +170,15 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(hall_B_int), hall_a_change, CHANGE);
   attachInterrupt(digitalPinToInterrupt(hall_C_int), hall_b_change, CHANGE);
   attachInterrupt(digitalPinToInterrupt(hall_A_int), hall_c_change, CHANGE);
+
+  attachInterrupt(digitalPinToInterrupt(r_hall_B_int), r_hall_a_change, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(r_hall_C_int), r_hall_b_change, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(r_hall_A_int), r_hall_c_change, CHANGE);
   //attachInterrupt(digitalPinToInterrupt(hall_B_int), hall_a_fall, FALLING);
   pinMode(LED, OUTPUT);
   Setpoint = 0.1;
 
-myPID.SetOutputLimits((-1)*max_PID_output, max_PID_output);
+  myPID.SetOutputLimits((-1)*max_PID_output, max_PID_output);
 
   //turn the PID on
   myPID.SetMode(AUTOMATIC);
@@ -134,20 +189,28 @@ long timer = millis();
 long timer_2 = millis();
 int inByte = 100;
 long last_counter = 0;
+long r_last_counter = 0;
 int speed_var = 300;
 int speed_var_2 = 0;
 float velocity = 0;
+float r_velocity = 0;
+int loop_delay = 2;
 void print_velocity() {
   if (((millis() - timer) > 175)) {
     velocity = float(((float)wheel_arc_per_tick * ((float)counter - (float)last_counter)) / (1000 * ((float)millis() - (float)timer))) * 10000.0;
+    r_velocity = float(((float)wheel_arc_per_tick * ((float)r_counter - (float)r_last_counter)) / (1000 * ((float)millis() - (float)timer))) * 10000.0;
+
     //Serial.print((counter - last_counter));
     //Serial.print("  ");
     //Serial.print(1000*(millis() - timer));
     Serial.print("velocity = ");
-    Serial.println(velocity * 1000);
+    Serial.print(velocity * 1000);
+    Serial.print(" r_velocity = ");
+    Serial.println(r_velocity * 1000);
     //Serial.print("\twheel_position = ");
     //Serial.println(wheel_arc_per_tick*(counter));
     last_counter = counter ;
+    r_last_counter = r_counter ;
     timer = millis();
   }
 }
@@ -155,7 +218,8 @@ bool s_flag = false;
 void loop() {
   //digitalWrite(LED, HIGH);
   delay(500);
-  float upto = 0.3;
+  //float upto = 0.3;
+  int upto = 800;
   while (1) {
     //    while (Serial.available() > 0) {
     //      // hoverboard.sendBuzzer((8), 1, 100, PROTOCOL_SOM_NOACK);
@@ -184,7 +248,7 @@ void loop() {
       }
     } else if (!s_flag)  {
       Setpoint -= 0.01;
-      if (Setpoint < (upto*-1)) {
+      if (Setpoint < (upto * -1)) {
         s_flag = true;
       }
     }
@@ -193,47 +257,42 @@ void loop() {
 
     float dt = ((float)millis() - (float)timer) * 1000.0;
     float dx = (((float)counter - (float)last_counter)) * (float)wheel_arc_per_tick;
-      dx = lowpassFilter.input( dx );
+    dx = lowpassFilter.input( dx );
 
-    Input = (dx/dt)*(-1000);
+    Input = (dx / dt) * (-1000);
     timer = millis();
     last_counter = counter;
-    
+
 
     //Input = float(((float)wheel_arc_per_tick * ((float)counter - (float)last_counter)) / (1000 * ((float)millis() - (float)timer))) * -10000.0;
-    myPID.Compute();
-    speed_var_2 = Output;
 
-    //    for (int i = 0; i < upto; i++) {
-    //      hoverboard.sendPWM(i, 0, PROTOCOL_SOM_NOACK);
-    //      print_velocity();
-    //    }
-    //
-    //
-    //    for (int i = upto; i > 0; i--) {
-    //      hoverboard.sendPWM(i, 0, PROTOCOL_SOM_NOACK);
-    //      print_velocity();
-    //    }
-    //
-    //    for (int i = 0; i < upto; i++) {
-    //      hoverboard.sendPWM((-1)*i, 0, PROTOCOL_SOM_NOACK);
-    //      print_velocity();
-    //    }
-    //
-    //    for (int i = upto; i > 0; i--) {
-    //      hoverboard.sendPWM((-1)*i, 0, PROTOCOL_SOM_NOACK);
-    //      print_velocity();
-    //    }
-    hoverboard.sendPWM(speed_var_2, speed_var_2, PROTOCOL_SOM_NOACK);
+
+    for (int i = 0; i < upto; i++) {
+      hoverboard.sendPWM(i, (-1)*i, PROTOCOL_SOM_NOACK);
+      delay(loop_delay);
+      print_velocity();
+    }
+
+
+    for (int i = upto; i > 0; i--) {
+      hoverboard.sendPWM(i, (-1)*i, PROTOCOL_SOM_NOACK);
+      delay(loop_delay);
+      print_velocity();
+    }
+
+    for (int i = 0; i < upto; i++) {
+      hoverboard.sendPWM((-1)*i, i, PROTOCOL_SOM_NOACK);
+      delay(loop_delay);
+      print_velocity();
+    }
+
+    for (int i = upto; i > 0; i--) {
+      hoverboard.sendPWM((-1)*i, i, PROTOCOL_SOM_NOACK);
+      delay(loop_delay);
+      print_velocity();
+    }
+    //hoverboard.sendPWM(speed_var_2, speed_var_2, PROTOCOL_SOM_NOACK);
     //hoverboard.sendPWMData(10, speed_var, 100, 50, 10, PROTOCOL_SOM_NOACK);
-
-    Serial.print("input: ");
-
-    Serial.print(Input);
-    Serial.print("setpoint: ");
-    Serial.print(Setpoint);
-    Serial.print("output: ");
-    Serial.println(Output);
 
     //
     //    //digitalWrite(LED, forward);
