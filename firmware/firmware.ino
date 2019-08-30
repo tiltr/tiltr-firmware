33e5b14f9552015ae37d75abbd0e820a0e1b4dcc
carebare47 <tom_q@hotmail.co.uk>
2019-08-30 14:50:21 +0000

    either works with timer and breaks loop or vice versa. Work on this to get interrupt timers working for IMU

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -3,43 +3,12 @@
 #include <hall_encoders.h>
 #include <HoverboardAPI.h>
 #include <Filters.h>
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
-
-MPU6050 mpu;
-
-#define OUTPUT_READABLE_YAWPITCHROLL
-
-#define INTERRUPT_PIN 9  // use pin 2 on Arduino Uno & most boards
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
-bool blinkState = false;
 
 #define LED PA5
 
 wheel_encoder left_encoder('L');
 wheel_encoder right_encoder('R');
 
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-  mpuInterrupt = true;
-}
-//HardwareSerial pc(USBTX, USBRX);
 HardwareSerial Serial4(PA1, PA0);
 
 int serialWrapper(unsigned char *data, int len) {
@@ -67,67 +36,44 @@ void r_hall_c_change() {
   right_encoder.hall_c_change();
 }
 
+void ISR_IMU_timer(HardwareTimer*)
+{
+  float imu_data = get_imu_data(0);
+  //Serial.println(imu_data);
+}
+
+
 void setup() {
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-  Wire.begin();
-  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-  Fastwire::setup(400, true);
-#endif
 
   // Comms to PC
   Serial.begin(115200);
   // Comms to hoverboard
   Serial4.begin(115200);
 
-  mpu.initialize();
-  pinMode(INTERRUPT_PIN, INPUT);
+  init_mpu();
 
-  bool mpu_connected = mpu.testConnection();
-  Serial.println(mpu_connected ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-  if (!mpu_connected) {
-    pinMode(LED_PIN, OUTPUT);
-    while (1) {
-      Serial.println("MPU6050 connection failed");
-      digitalWrite(LED_PIN, HIGH);
-      delay(200);
-      digitalWrite(LED_PIN, LOW);
-      delay(200);
-    }
-  }
+#if defined(TIM1)
+  TIM_TypeDef *Instance = TIM1;
+#else
+  TIM_TypeDef *Instance = TIM2;
+#endif
 
-  Serial.println(F("Initializing DMP..."));
-  devStatus = mpu.dmpInitialize();
+  // Instantiate HardwareTimer object. Thanks to 'new' instanciation, HardwareTimer is not destructed when setup() function is finished.
+  HardwareTimer *MyTim = new HardwareTimer(Instance);
 
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXGyroOffset(220);
-  mpu.setYGyroOffset(76);
-  mpu.setZGyroOffset(-85);
-  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
 
-  // make sure dmp stuff worked (returns 0 if so)
-  if (devStatus == 0) {
-    mpu.setDMPEnabled(true);
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
-    Serial.println(F("DMP ready! Waiting for first interrupt..."));
-    dmpReady = true;
-    // get expected DMP packet size for later comparison
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-    Serial.print(F("DMP Initialization failed (code "));
-    Serial.print(devStatus);
-    Serial.println(F(")"));
-  }
+  MyTim->setMode(2, TIMER_OUTPUT_COMPARE);  // In our case, channekFalling is configured but not really used. Nevertheless it would be possible to attach a callback to channel compare match.
+  MyTim->setOverflow(400, HERTZ_FORMAT); // 10 Hz
+  MyTim->attachInterrupt(ISR_IMU_timer);
+  MyTim->resume();
+
+
+
+
+
 
   // configure LED for output
-  pinMode(LED_PIN, OUTPUT);
+
 
   attachInterrupt(digitalPinToInterrupt(left_encoder.hall_B_int), l_hall_a_change, CHANGE);
   attachInterrupt(digitalPinToInterrupt(left_encoder.hall_C_int), l_hall_b_change, CHANGE);
@@ -151,8 +97,8 @@ void print_velocity() {
     Serial.print(left_encoder.get_velocity() * 1000);
     Serial.print(" r_velocity = ");
     Serial.print(right_encoder.get_velocity() * 1000);
-    Serial.print(" , angle: ");
-    Serial.println(ypr[1] * 180 / M_PI);
+    Serial.println(" , angle: ");
+    //Serial.println(get_imu_data(1));
     timer = millis();
   }
 }
@@ -180,69 +126,21 @@ void test_motors(int upto, int loop_delay) {
 }
 bool s_flag = false;
 void loop() {
-  // if DMP programming failed, don't try to do anything
-  if (!dmpReady) return;
 
-  // wait for MPU interrupt or extra packet(s) available
-  while (!mpuInterrupt && fifoCount < packetSize) {
-    if (mpuInterrupt && fifoCount < packetSize) {
-      // try to get out of the infinite loop
-      fifoCount = mpu.getFIFOCount();
-    }
-    // other program behavior stuff here
+  print_velocity();
 
-    //   test_motors(600, 2);
-    print_velocity();
+//  if (((millis() - timer_2) > 1000) && p_flag) {
+//    //void HoverboardAPI::sendBuzzer(uint8_t buzzerFreq, uint8_t buzzerPattern, uint16_t buzzerLen, char som)
+//    hoverboard.sendBuzzer(10, 1, 100, PROTOCOL_SOM_NOACK);
+//    Serial4.println("unlockASCII");
+//    Serial4.println('P');
+//    p_flag = false;
+//  }
+  //
+  //  for (int i = 0; i < 3; i++) {
+  //    Serial.print(get_imu_data(i));
+  //  }
+  //  Serial.println("");
 
-    if (((millis() - timer_2) > 1000) && p_flag) {
-      //void HoverboardAPI::sendBuzzer(uint8_t buzzerFreq, uint8_t buzzerPattern, uint16_t buzzerLen, char som)
-      hoverboard.sendBuzzer(10, 1, 100, PROTOCOL_SOM_NOACK);
-      Serial4.println("unlockASCII");
-      Serial4.println('P');
-      p_flag = false;
-    }
-
-  }
-
-
-  // reset interrupt flag and get INT_STATUS byte
-  mpuInterrupt = false;
-  mpuIntStatus = mpu.getIntStatus();
-
-  // get current FIFO count
-  fifoCount = mpu.getFIFOCount();
-
-  // check for overflow (this should never happen unless our code is too inefficient)
-  if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
-    // reset so we can continue cleanly
-    mpu.resetFIFO();
-    fifoCount = mpu.getFIFOCount();
-    Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-  } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
-    // wait for correct available data length, should be a VERY short wait
-    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-    // read a packet from FIFO
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-    // track FIFO count here in case there is > 1 packet available
-    // (this lets us immediately read more without waiting for an interrupt)
-    fifoCount -= packetSize;
-
-
-#ifdef OUTPUT_READABLE_YAWPITCHROLL
-    // display Euler angles in degrees
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    //Serial.println(ypr[1] * 180 / M_PI);
-#endif
-
-    // blink LED to indicate activity
-    blinkState = !blinkState;
-    digitalWrite(LED_PIN, blinkState);
-  }
 
 }
